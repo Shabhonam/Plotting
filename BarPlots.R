54a60c99b0756d04f2ab79c6979cafddfffae09b
Shabhonam <shabhonam_caim@yahoo.co.in>
2017-11-09 13:17:46 +0000

    Add files via upload

---
+++ BarPlots.R
@@ -0,0 +1,19 @@
+setwd("~/Desktop/Testdata/NewcastleData/")
+library(ggplot2)
+Stack_data <- read.csv("tiny-test.csv")
+png("Taxon.png",
+    width = 5*3000,        # 5 x 300 pixels
+    height = 5*3000,
+    res = 600,            # 300 pixels per inch
+    pointsize = 10)
+
+library(reshape2)
+df_long <- melt(Stack_data, id.vars = "ID", variable.name = "Phyla")
+
+
+library(ggplot2)
+#pdf("test.pdf")
+ggplot(df_long, aes(x = ID, y = value, fill = Phyla)) + 
+  geom_bar(stat = "identity")+theme(legend.position="bottom", legend.direction="horizontal", legend.title = element_blank())
+
+dev.off()
