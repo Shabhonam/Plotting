af00beedf284eb59748d7fdfe2ec466d20fcb588
Shabhonam <shabhonam_caim@yahoo.co.in>
2017-12-01 10:57:54 +0000

    Add files via upload

---
+++ Simple_PCA.R
@@ -0,0 +1,29 @@
+setwd("~/Desktop/Testdata/Mel/PCA_Mel/")
+library(ggfortify)
+library(cluster)
+require("gplots")
+require("ggplot2")
+require("RColorBrewer")
+data <- read.csv("NMDS_data_mel.csv")
+head(data[, 1:7])
+
+df <- data[c(2:87)]
+
+
+
+png("PCA_test.png",
+    width = 5*300,        # 5 x 300 pixels
+    height = 5*300,
+    res = 300,            # 300 pixels per inch
+    pointsize = 10)
+
+autoplot(prcomp(df), data = data, colour = 'ID')+
+  theme(legend.position="bottom", legend.direction="horizontal", legend.title = element_blank())
+autoplot(fanny(df, 4), frame = TRUE,  )
+dev.off()
+
+
+
+
+
+
